0211d36f9ee00f5be6a87c184c39567b539dc9e4
bobbadi veera venkata subrahmanyam <subbubest001@gmail.com>
2026-04-09 22:51:26 +0530

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,15 @@
+{
+  "name": "Reminder App",
+  "short_name": "Reminder",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#000000",
+  "theme_color": "#000000",
+  "icons": [
+    {
+      "src": "icon.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    }
+  ]
+}
